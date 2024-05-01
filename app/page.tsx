0503e45539a4c9d7e2dd5fba3925379a9e9f0f20
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-05-01 14:48:56 -0300

    feat: add product list and product components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
+import ProductsList from "@/components/products-list";
 import Search from "@/components/search";
+import { Button } from "@/components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,7 +11,7 @@ export default function Home() {
     <>
       <Header />
 
-      <div className="pt6 px-5">
+      <div className="px-5 pt-6">
         <Search />
       </div>
 
@@ -26,6 +29,21 @@ export default function Home() {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            variant="ghost"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+
+        <ProductsList />
       </div>
     </>
   );
