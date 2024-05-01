3a8eb009558f3e17a1ee9f1ee6fae5737eea203e
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-05-01 00:33:39 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +14,18 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="pt-6">
+        <Image
+          src="/promoBanner.svg"
+          alt="Até 30% por cento de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
+      <Image src="/logo.svg" alt="FSW Foods" height={30} width={100} />
 
       <Button
         size="icon"
